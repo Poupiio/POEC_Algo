d2104097adb85009e7db7f0363b3e2ee7e0c2a69
Elise <elise.maaiza33@gmail.com>
2024-02-19 15:04:38 +0100

    add advances exercises

---
+++ Avancé/index.js
@@ -0,0 +1,55 @@
+// ------------------------------------------------------------------------------------ //
+//                                      EXERCICE 1                                      //
+// ------------------------------------------------------------------------------------ //
+/* Déterminez à partir de l'âge si une personne est :
+Enfant (en dessous ou égale à 10 ans)
+Adolescent (entre 11 et 17 ans)
+Majeur (au-dessus de 18 ans)
+Senior (au-dessus de 50 ans) */
+
+const determineAge = (age) => {
+    if (age <= 10) {
+        return "Enfant";
+    } else if (age >= 11 && age <= 17) {
+        return "Adolescent";
+    } else if (age >= 18 && age <= 49) {
+        return "Majeur";
+    } else {
+        return "Senior";
+    }
+}
+determineAge(15);
+
+
+// ------------------------------------------------------------------------------------ //
+//                                      EXERCICE 2                                      //
+// ------------------------------------------------------------------------------------ //
+/* Calculez la moyenne de plusieurs notes
+Spécifications techniques
+Une fonction qui prend en paramètre un tableau et renvoie la moyenne */
+
+let notes = [12, 8, 20, 10];
+
+function avgCalculator(notes) {
+    let sum = 0;
+
+    for (let i = 0; i < notes.length; i++) {
+        sum += notes[i];
+    }
+
+    let avg = sum / notes.length;
+
+    return avg;
+}
+
+console.log(avgCalculator(notes));
+
+
+// ------------------------------------------------------------------------------------ //
+//                                      EXERCICE 3                                      //
+// ------------------------------------------------------------------------------------ //
+/* Calculez le prix TTC d'un produit
+Spécifications techniques
+Prix hors taxes et taux de la tva en entrée (paramètres de votre fonction)
+La fonction retourne le prix TTC */
+
