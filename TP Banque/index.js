573385dff3b14f196902ecca2c35d233f91ecb96
Elise <elise.maaiza33@gmail.com>
2024-02-19 22:28:14 +0100

    modified display

--- TP Banque/index.js
+++ TP Banque/index.js
@@ -1,33 +1,44 @@
+const overdraftContainer = document.querySelector('.overdraft-container');
+const amountContainer = document.querySelector('.amount-container');
 const overdraft = document.getElementById('overdraft');
 const amount = document.getElementById('amount');
 const overdraftAlert = document.getElementById('overdraft-alert');
 const amountAlert = document.getElementById('amount-alert');
+const allowedOverdraft = document.getElementById('allowed-overdraft');
+const transfer = document.getElementById('transfer');
+const recap = document.querySelector('.recap-container');
 
 let isOverdraft = false;
 
 document.body.addEventListener('keydown', (e) => {
     if (e.key == "y") {
         isOverdraft = true;
+        overdraftContainer.style.display = "block";
+        amountContainer.style.display = "block";
+        recap.style.display = "block";
     }
 });
 
 overdraft.addEventListener('input', (e) => {
-    console.log(e.target.value);
+    let overdraftValue = Number(e.target.value);
 
-    if (e.target.value < 100 || e.target.value > 2000) {
+    if ((e.target.value < 100 || e.target.value > 2000) && isOverdraft) {
         overdraftAlert.style.display = "block";
     } else {
         overdraftAlert.style.display = "none";
+        allowedOverdraft.textContent = overdraftValue;
     }
 })
 
 amount.addEventListener('input', (e) => {
-    console.log(e.target.value);
+    let amountValue = Number(e.target.value);
+    console.log(amountValue);
 
     if (e.target.value < 500) {
         amountAlert.style.display = "block";
     } else {
         amountAlert.style.display = "none";
+        transfer.textContent = amountValue;
     }
 })
 
